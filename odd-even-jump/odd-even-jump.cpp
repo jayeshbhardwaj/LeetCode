46cd6272d0eeeb36d3cad06088a6a2f472f17364
jayeshb <bhardwajjayesh7@gmail.com>
2021-11-02 14:06:29 +0530

    Time: 80 ms (81.05%), Space: 27.4 MB (54.96%) - LeetHub

--- odd-even-jump/odd-even-jump.cpp
+++ odd-even-jump/odd-even-jump.cpp
@@ -1,58 +1,22 @@
 class Solution {
+    
 public:
     int oddEvenJumps(vector<int>& arr) {
       int res = 1;
       int n = arr.size();
-      vector<int> index(n);
-      for(int i=0 ;i < n;i++)index[i]=i;
-      auto cmp_odd = [&arr](const int x, const int y){
-        return arr[x] < arr[y] || (arr[x] == arr[y] && x < y) ;
-      };
-      auto cmp_even = [&arr](const int x, const int y){
-        return arr[x] > arr[y] || (arr[x] == arr[y] && x < y) ;
-      };
-      
-      auto next_higher = [](vector<int>& index) -> vector<int> {
-        int n = index.size();
-        stack<int> s;
-        vector<int> next_higher(n);
-        int i = 0;
-        for(int i = 0; i < n; i++){
-          while(!s.empty() && index[i] > s.top()){
-            next_higher[s.top()] = index[i];
-            s.pop();
-          }
-          s.push(index[i]);
-        }
-        while(!s.empty()){
-          next_higher[s.top()] = -1;
-          s.pop();
-        }
-        return next_higher;
-      };
-      
-      sort(index.begin(),index.end(),cmp_odd);
-      vector<int> odd_next = next_higher(index);
-      sort(index.begin(),index.end(),cmp_even);
-      vector<int> even_next = next_higher(index);
-      
-      vector<bool> odd(n);
-      vector<bool> even(n);
-      odd[n-1]=true;
-      even[n-1]=true;
-      
+      map<int,int> num;
+      vector<int> odd(n);
+      vector<int> even(n);
+      odd[n-1]=even[n-1]=1;
+      num.insert(make_pair(arr[n-1],n-1));
       for(int i = n-2; i>=0 ; i--){
-        //using bounds in map
-        //auto hi = num.lower_bound(arr[i]), lo = num.upper_bound(arr[i]); 
-        //if(hi!=num.end())odd[i]=even[hi->second];
-        //if(lo!=num.begin())even[i]=odd[(--lo)->second];
-        auto hi = odd_next[i], lo = even_next[i];
-        if(hi!=-1)odd[i]=even[hi];
-        if(lo!=-1)even[i]=odd[lo];
+        auto hi = num.lower_bound(arr[i]), lo = num.upper_bound(arr[i]); 
+        if(hi!=num.end())odd[i]=even[hi->second];
+        if(lo!=num.begin())even[i]=odd[(--lo)->second];
         if(odd[i])res++;  
+        num[arr[i]]=i;
       } 
       return res;
     }
    
-  
 };
